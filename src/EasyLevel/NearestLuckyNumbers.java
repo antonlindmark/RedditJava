3d49f78144a54ccb506757b5699efde3f19c7371
Anton Lindmark <Anton Lindmark>
2017-09-04 14:12:30 +0200

    hello world added

---
+++ src/EasyLevel/NearestLuckyNumbers.java
@@ -0,0 +1,12 @@
+package EasyLevel;
+
+/**
+ * Created by user on 2017-09-04.
+ */
+public class NearestLuckyNumbers {
+
+    public static void main(String [] args){
+
+        System.out.println("Hello first problem!");
+    }
+}
